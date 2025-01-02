c91c2c2cca920ce9fda8c2f34a29e0b730aa9239
exodriv <90026735+exodriv@users.noreply.github.com>
2025-01-02 16:17:24 +0000

    inlined easy_clustered function as well, hard function already

--- src/lib.rs
+++ src/lib.rs
@@ -135,7 +135,7 @@ pub fn special_leaves_type_1(
         m1[b] -= 1;
     }
     let criterion = reg_var.1 / pp;
-    while m1[b] > criterion {// *pp > reg_var.1 {
+    while m1[b] > criterion {
       let y = (reg_var.0 / (m1[b] as u64 * pp as u64)) as usize ;
       if y >=intervals.1[intervals.0+1] {return;} 
       let muvalue = mu[(m1[b] + 1) >> 1];
@@ -147,70 +147,6 @@ pub fn special_leaves_type_1(
         m1[b] -= 2;
       }
       }
-
-
-#[inline]
-pub fn hard(intervals: Intervals, reg_var: &mut RegVars, y: usize, d2_index: &mut usize) {
-        *reg_var.2 += cnt_query(y - intervals.1[intervals.0], reg_var.3) as i64;
-        *d2_index -= 1;
-}
-
-#[inline]
-pub fn easy_sparse(
-    index: usize,
-    reg_var: &mut RegVars,
-    s2b_var: &mut S2bVars,
-    y: usize,
-    tt: &mut [u8],
-    switch: &mut [bool],
-) -> bool {
-    if y < reg_var.1 {
-        let l = s2b_var.1[(y + 1) >> 1] - index + 1;
-        *reg_var.2 += l as i64;
-        *s2b_var.0 -= 1;
-        false
-    } else if switch[index] {
-        tt[index] = 2;
-        false
-    }
-        else {
-            switch[index] = true;
-        true
-    }
-}
-
-#[inline]
-pub fn easy_clustered(
-    index: usize,
-    reg_var: &mut RegVars,
-    s2b_var: &mut S2bVars,
-    y: usize,
-    tt: &mut [u8],
-    switch: &mut [bool],
-) -> bool {
-    if y < reg_var.1 {
-        let l = s2b_var.1[(y + 1) >> 1] - index + 1;
-        let term = reg_var.0 / (s2b_var.2[index + 1] as u64 * s2b_var.2[index + l] as u64);
-        let dprime = s2b_var.1[((term + 1) >> 1) as usize];
-        if s2b_var.2[dprime + 1] <= int_sqrt((reg_var.0 / s2b_var.2[index + 1] as u64) as usize)
-            || dprime <= index
-        {
-            tt[index] = 1;
-            *reg_var.2 += l as i64;
-            *s2b_var.0 -= 1;
-        } else {
-            *reg_var.2 += (l as u32 * (*s2b_var.0 - dprime) as u32) as i64;
-            *s2b_var.0 = dprime;
-        }
-        false
-    } else if switch[index] {
-        tt[index] = 2;
-        false
-    } else {
-        switch[index] = true;
-        true
-    }
-}
 
 #[inline]
 pub fn special_leaves_type_2(
@@ -228,15 +164,29 @@ pub fn special_leaves_type_2(
         // if y < cmp::max(reg_var.1 , intervals.1[intervals.0+1]) {
         match tt[index] {
             0 => {
-                let easy_c: bool =
-                    easy_clustered(index, reg_var, s2b_var, y, tt, switch);
-                if easy_c {
+                if bitn {
+                    let l = s2b_var.1[(y + 1) >> 1] - index + 1;
+                    let term = reg_var.0 / (s2b_var.2[index + 1] as u64 * s2b_var.2[index + l] as u64);
+                    let dprime = s2b_var.1[((term + 1) >> 1) as usize];
+                    if s2b_var.2[dprime + 1] <= int_sqrt((reg_var.0 / s2b_var.2[index + 1] as u64) as usize)
+                        || dprime <= index
+                    {
+                        tt[index] = 1;
+                        *reg_var.2 += l as i64;
+                        *s2b_var.0 -= 1;
+                    } else {
+                        *reg_var.2 += (l as u32 * (*s2b_var.0 - dprime) as u32) as i64;
+                        *s2b_var.0 = dprime;
+                    }
+                } else if switch[index] {
+                    tt[index] = 2;
+                } else {
+                    switch[index] = true;
                     break;
                 }
-            }
+            },
             1 => {
                 if bitn {
-                // if y < reg_var.1 {
                     let l = s2b_var.1[(y + 1) >> 1] - index + 1;
                     *reg_var.2 += l as i64;
                     *s2b_var.0 -= 1;
@@ -247,11 +197,7 @@ pub fn special_leaves_type_2(
                     switch[index] = true;
                     break;
                 }
-                // let easy_s: bool = easy_sparse(index, reg_var, s2b_var, y, tt, switch);
-                // if easy_s {
-                //     break;
-                // }
-            }
+            },
             _ => {
                 let bit = y<intervals.1[intervals.0 + 1];
                 if bit
@@ -263,7 +209,7 @@ pub fn special_leaves_type_2(
                else if intervals.0 > 0 || reg_var.3[1] > 0 {
                     break;
                 } 
-            }
+            },
         }
 }
     s2primes
